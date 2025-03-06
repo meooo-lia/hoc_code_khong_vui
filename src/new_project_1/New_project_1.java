bc169153efe7f6b8957ffa3f3ef5c4fdaa986089
meooo-lia <anhduytran4089@gmail.com>
2025-03-06 19:51:37 +0700

    Project_1

---
+++ src/new_project_1/New_project_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package new_project_1;
+
+/**
+ *
+ * @author bibau
+ */
+public class New_project_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
